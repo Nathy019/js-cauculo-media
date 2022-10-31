5fc687dd62cf002e9aee254a858d8021b9f1a271
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2022-10-31 19:39:36 -0300

    JS calculo média

---
+++ app.js
@@ -0,0 +1,16 @@
+//console.log("Olá, estou funcionando")
+//console.log(10+3-5)
+//console.log(10-4)
+//console.log(8*6)
+//console.log(42/2)
+
+let nome = "Nathy"
+let nota1 = 9
+let nota2 = 7
+let nota3 = 8
+let nota4 = 2
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+let mediaFixada = media.toFixed(1)
+
+console.log("Olá " + nome)
+console.log("Sua media foi:" + mediaFixada)
